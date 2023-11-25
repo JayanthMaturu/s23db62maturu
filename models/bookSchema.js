ea1876aceb7b1753b440a45ebc048682c1912f16
JayanthMaturu <S566162@nwmissouri.edu>
2023-11-24 18:36:43 -0600

    part6

--- models/bookSchema.js
+++ models/bookSchema.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 const bookSchema = mongoose.Schema({
     book_name: String,
     book_author: String,
-    book_cost: Number
+    book_cost: {
+        type: Number,
+        min: 5,
+        max: 50
+    } 
 })
 module.exports = mongoose.model("book",
 bookSchema)
--- routes/book.js
+++ routes/book.js
@@ -15,13 +15,13 @@ router.get('/',book_controlers.book_view_all_Page);
 /* GET detail book page */
 router.get('/detail', book_controlers.book_view_one_Page);
 
-/* GET create costume page */
+/* GET create book page */
 router.get('/create', book_controlers.book_create_Page);
 
-/* GET create update page */
-router.get('/update', book_controlers.book_update_Page);
+/* GET update  book page */
+router.get('/update',secured, book_controlers.book_update_Page);
 
-/* GET delete costume page */
+/* GET delete book page */
 router.get('/delete', book_controlers.book_delete_Page);
 
 module.exports = router;
